90cd57a10154e8cd9ca57be96558ce04b11f0417
João D'Amorim <joao.damorim@gmail.com>
2018-07-29 16:52:15 -0300

    Métodos qtd_de_pessoas_em_cada_local e calcular_matriz_de_transicao.

--- Markov/markov.py
+++ Markov/markov.py
@@ -38,6 +38,10 @@ x2 = np.matrix([
 x = np.dot(x2, x1)
 print(x)
 
+dic = {}
+dic['lc1'] = ['k', 'l']
+print(dic['lc1'])
+
 class Markov:
 
     def qtd_de_pessoas(self, lista_pessoa):
@@ -53,9 +57,32 @@ class Markov:
             if pessoa['local'] == 'local_1':
                 localizacao['local_1'].append(pessoa)
             elif pessoa['local'] == 'local_2':
-                localizacao['local_1'].append(pessoa)
+                localizacao['local_2'].append(pessoa)
             elif pessoa['local'] == 'local_3':
-                localizacao['local_1'].append(pessoa)
+                localizacao['local_3'].append(pessoa)
         return localizacao
 
+    def qtd_de_pessoas_em_cada_local(self, dicionario_locais_pessoas):
+        qtd_pessoas_local = {}
+        local_1 = len(dicionario_locais_pessoas['local_1'])
+        qtd_pessoas_local['local_1'] = local_1
+        local_2 = len(dicionario_locais_pessoas['local_2'])
+        qtd_pessoas_local['local_2'] = local_2
+        local_3 = len(dicionario_locais_pessoas['local_3'])
+        qtd_pessoas_local['local_3'] = local_3
+        return qtd_pessoas_local
 
+
+
+    def calcular_matriz_de_transicao(self, qtd_pessoas_local, localizacao_pessoa_inicial_1_min, localizacao_pessoa_60_min):
+        total_de_pessoas_local_1 = qtd_pessoas_local['local_1']
+        total_de_pessoas_local_2 = qtd_pessoas_local['local_2']
+        total_de_pessoas_local_3 = qtd_pessoas_local['local_3']
+
+        for
+        matriz = np.matrix([
+            [a11, a12, a13],
+            [a21, a22, a23],
+            [a31, a32, a33]
+        ])
+        return
